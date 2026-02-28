92ff77e40fc6745be808fb17336b352bdc24a929
wrudman <william.rudman@utexas.edu>
2026-02-28 12:31:03 -0600

    garden?

--- assets/js/garden.js
+++ assets/js/garden.js
@@ -300,6 +300,8 @@
 
 // turn the flat representation into a tree
 
+var flowerTemplateHTML = $('#flower_template').html();
+
 var garden_height = 580;
 var plantWidth = 400;
 var root_stem_height = 70;
@@ -312,7 +314,6 @@ var research_garden = [];
 var id2paper = {};
 
 function build_garden(papers) {
-    var flowerTemplateHTML = $('#flower_template').html();
     research_garden = [];
     id2paper = {};
     for(var paper of papers) {
