8fd9397f3f86b4c9f5aa6e0a151798a24b713603
wrudman <william.rudman@utexas.edu>
2026-09-18 09:35:43 -0500

    add space

--- assets/js/garden.js
+++ assets/js/garden.js
@@ -303,9 +303,9 @@
 var flowerTemplateHTML = $('#flower_template').html();
 
 var garden_height = 580;
-var plantWidth = 400;
+var plantWidth = 560;
 var root_stem_height = 70;
-var root_offset = 135;
+var root_offset = 90;
 var stem_height = 80;
 
 const waveWidth = 150, wave_offset = 80, waveHeight = 50;
